1b4ffba24eba6a7be5903b70ec1aa2f4c66ac8c8
Diogo C Brunoro <161259842+DiogoBrunoro@users.noreply.github.com>
2025-03-26 16:14:50 -0300

    Add files via upload

---
+++ Desafio 6/Desafio  6/Desafio__6App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio__6App.swift
+//  Desafio  6
+//
+//  Created by Turma01-7 on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Desafio__6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
